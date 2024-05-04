6f5d04ecfcf427b1cbe7b5341a323f0014b07720
Renan <renan.economia.2011@gmail.com>
2024-05-04 18:48:27 -0400

    Adição do Resumo booleans

--- tipos-variáveis/src/TiposVariaveis.java
+++ tipos-variáveis/src/TiposVariaveis.java
@@ -1,5 +1,34 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     
+        //Resumo para booleans
+
+        int a = 6;
+        int b = 6;
+
+        String resultado = "";
+
+        if(a==b)
+            resultado = "Verdadeiro";
+
+        else
+            resultado = "Falso";
+
+        
+            System.out.println(resultado);
+
+
+
+        //Possibilidade 2 - Resumida
+
+        int c = 6;
+        int d = 5;
+
+        String resultado2 = c==d ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado2);
+
+
+        
     }
 }
