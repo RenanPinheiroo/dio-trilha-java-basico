80b632655f6a112fc4bd1ecad401e1693a81f848
Renan <renan.economia.2011@gmail.com>
2024-05-05 21:50:32 -0400

    Adição da Orientação ao Objeto

---
+++ tipos-classes/src/SmartTv.java
@@ -0,0 +1,17 @@
+public class SmartTv {  
+
+    boolean ligada=false;
+    int canal = 4;
+    int volume = 25;
+
+    public void ligar(){
+
+        ligada=true;
+    }
+
+        public void desligar(){
+
+        ligada=false;
+    }
+    
+}
---
+++ tipos-classes/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+        System.out.println("A Tv está ligada?" + smartTv.ligada);
+        System.out.println("O canal é: "+ smartTv.canal);
+        System.out.println("O volume atual é: " + smartTv.volume);
+    
+        smartTv.ligar ();
+
+        System.out.println("A Tv está ligada? " + smartTv.ligada);
+    }
+}
